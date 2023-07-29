9aa0c98cdb21d93b2e60db61f525e9c407302cf9
YuliiaDubyniuk <126833026+YuliiaDubyniuk@users.noreply.github.com>
2023-07-29 12:31:09 -0400

    hw 8 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+    
+const LS_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(saveCurrentPlayTime, 1000));
+
+function saveCurrentPlayTime(data) {
+    localStorage.setItem(LS_KEY, data.seconds);
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(LS_KEY))||0);
+  
+
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+
+const LS_KEY = 'feedback-form-state';
+const { email, message } = formEl.elements;
+
+const savedData = checkStorage();
+if (savedData) {
+  email.value = savedData.email || '';
+  message.value = savedData.message || '';
+}
+
+function onInput() {
+  formData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem(LS_KEY, JSON.stringify(formData) || {});
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  if (email.value.trim() === '' || message.value.trim() === '') {
+    return alert('Please, fill out all the fields!');
+  }
+  console.log(formData);
+  localStorage.removeItem(LS_KEY);
+  evt.currentTarget.reset();
+}
+
+function checkStorage() {
+  try {
+    const formData = localStorage.getItem(LS_KEY);
+    return formData ? JSON.parse(formData) : {};
+  } catch (err) {
+    console.log(err.message);
+  }
+}
